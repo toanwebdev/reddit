0499fb7360e9f617d4ec2970697347007109ec6d
toanwebdev <toanwebdev@gmail.com>
2022-01-01 10:27:53 +0700

    deloy web

--- client/src/lib/apolloClient.ts
+++ client/src/lib/apolloClient.ts
@@ -38,7 +38,7 @@ function createApolloClient(headers: IncomingHttpHeaders | null = null) {
   }
 
   const httpLink = new HttpLink({
-    uri: process.env.NODE_ENV === 'production' ? 'domain' : 'http://localhost:4000/graphql', // Server URL (must be absolute)
+    uri: process.env.NODE_ENV === 'production' ? 'https://pacific-beyond-59946.herokuapp.com/graphql' : 'http://localhost:4000/graphql', // Server URL (must be absolute)
     credentials: 'include', // Additional fetch() options like `credentials` or `headers`
     fetch: enhanceFetch
   })
